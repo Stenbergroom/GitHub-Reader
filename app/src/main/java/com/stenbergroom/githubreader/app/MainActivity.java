26165e952220e75a7ccb63c6482607793eb95f94
Aleksandr Marchenko <marrchenko@gmail.com>
2015-07-17 16:12:25 +0300

    creating project structure and test connection to git

---
+++ app/src/main/java/com/stenbergroom/githubreader/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.stenbergroom.githubreader.app;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/stenbergroom/githubreader/app/UserActivity.java
@@ -0,0 +1,53 @@
+package com.stenbergroom.githubreader.app;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+
+public class UserActivity extends Activity {
+
+    String account = "Stenbergroom";
+    private GitHub gitHub;
+    private TextView tv_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tv_test = (TextView)findViewById(R.id.tv_test);
+
+        try {
+            gitHub = GitHub.connectUsingOAuth(System.getProperty("github.oauth"));
+        } catch (IOException e) {
+            Toast.makeText(UserActivity.this, "Error Connect", Toast.LENGTH_SHORT).show();
+        }
+
+        new MyTask().execute();
+    }
+
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        GHUser user;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                user = gitHub.getUser(account);
+            } catch (IOException e) {
+                Toast.makeText(UserActivity.this, "Error doInBackground", Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            tv_test.setText(user.getLogin());
+        }
+    }
+}
