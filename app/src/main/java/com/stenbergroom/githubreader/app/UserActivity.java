366403070b02e3a06e73fd935b82d3b710db5ad1
Aleksandr Marchenko <marrchenko@gmail.com>
2015-07-21 20:16:39 +0300

    UserActivity design

--- app/src/main/java/com/stenbergroom/githubreader/app/UserActivity.java
+++ app/src/main/java/com/stenbergroom/githubreader/app/UserActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.nispok.snackbar.Snackbar;
@@ -35,6 +36,26 @@ public class UserActivity extends ActionBarActivity {
             Snackbar.with(UserActivity.this)
                     .text(MainActivity.getUser().getLogin())
                     .show(UserActivity.this);
+        }
+    }
+
+    public void onClickImage(View view) {
+        switch (view.getId()) {
+            case R.id.image_global:
+                Snackbar.with(UserActivity.this)
+                        .text("Global")
+                        .show(UserActivity.this);
+                break;
+            case R.id.image_share:
+                Snackbar.with(UserActivity.this)
+                        .text("Share")
+                        .show(UserActivity.this);
+                break;
+            case R.id.image_save:
+                Snackbar.with(UserActivity.this)
+                        .text("Save")
+                        .show(UserActivity.this);
+                break;
         }
     }
 
