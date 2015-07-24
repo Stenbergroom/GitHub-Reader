70c543d7dda830785d2abec5824a8ba84d92b47e
Aleksandr Marchenko <marrchenko@gmail.com>
2015-07-24 03:02:11 +0300

    changes in project structure, refactoring

--- app/src/main/java/com/stenbergroom/githubreader/app/helper/UserRepositoryHelper.java
+++ app/src/main/java/com/stenbergroom/githubreader/app/helper/UserRepositoryHelper.java
@@ -2,14 +2,10 @@ package com.stenbergroom.githubreader.app.helper;
 
 import android.os.AsyncTask;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
-import com.stenbergroom.githubreader.app.R;
-import com.stenbergroom.githubreader.app.UserActivity;
 import com.stenbergroom.githubreader.app.adapter.RepositoryAdapter;
-import com.stenbergroom.githubreader.app.animator.CustomAnimator;
 import com.stenbergroom.githubreader.app.entity.Repository;
 import com.stenbergroom.githubreader.app.entity.User;
 import org.kohsuke.github.GHRepository;
@@ -45,20 +41,12 @@ public class UserRepositoryHelper {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-//            progressBar = (ProgressBar)activity.findViewById(R.id.progressBar);
-//            swipeRefreshLayout = (SwipeRefreshLayout)activity.findViewById(R.id.swipe_container);
-//            recyclerView = (RecyclerView)activity.findViewById(R.id.list);
-//            recyclerView.setLayoutManager(new LinearLayoutManager(activity));
-//            recyclerView.setItemAnimator(new CustomAnimator());
-//            repositoryAdapter = new RepositoryAdapter(new ArrayList<Repository>(), R.layout.row_repository, activity);
-//            recyclerView.setAdapter(repositoryAdapter);
             repositoryAdapter.clearRepositories();
         }
 
         @Override
         protected Void doInBackground(Void... params) {
             repositoryList.clear();
-
             try {
                 Map<String, GHRepository> repositories = User.getGhUser().getRepositories();
                 for(String repoName : repositories.keySet()) {
@@ -77,9 +65,7 @@ public class UserRepositoryHelper {
             super.onPostExecute(aVoid);
             recyclerView.setVisibility(View.VISIBLE);
             progressBar.setVisibility(View.GONE);
-
             repositoryAdapter.addRepositories(repositoryList);
-
             swipeRefreshLayout.setRefreshing(false);
         }
     }
